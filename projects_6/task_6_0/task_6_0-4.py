59f5f69abc652086bcdf779705890906a56d8dc9
Толкачев Андрей Дмитриевич <tolkachevb6d8@gmail.com>
2026-05-15 08:29:32 +0300

    task_6_0-4.py

--- projects_6/task_6_0/task_6_0-4.py
+++ projects_6/task_6_0/task_6_0-4.py
@@ -4,7 +4,6 @@ df = pd.read_csv('wild_boars.csv')
 
 with open('task_6_0-4_modes.txt', 'w', encoding='utf-8') as f:
     for col in df.columns:
-        # Пропуск id
         if 'id' in col.lower():
             f.write(f"{col}: Мода не применима \n")
             continue
@@ -13,8 +12,8 @@ with open('task_6_0-4_modes.txt', 'w', encoding='utf-8') as f:
         if mode_series.empty:
             mode_str = "Нет моды"
         else:
-        
-      if len(mode_series) == len(df[col].dropna()):
+            # здесь отступ должен быть одинаковым (4 пробела)
+            if len(mode_series) == len(df[col].dropna()):
                 mode_str = "Все значения уникальные, мода не определима"
             else:
                 mode_str = ", ".join(map(str, mode_series.values))
